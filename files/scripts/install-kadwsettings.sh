70fde7eefa4ef1374608995823ce774d24895367
jahinzee <jahinzee@proton.me>
2025-09-20 12:46:37 +1000

    pkg: add kadwsettings

---
+++ files/scripts/install-kadwsettings.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -oue pipefail
+
+SOURCE_REPO="https://github.com/Hou-Rui/kadwsettings"
+
+dnf install -y \
+    python3 \
+    python-pyside6 \
+    kf6-kirigami \
+    kf6-kirigami-addons \
+    git
+
+tempdir="$(mktemp -d)"
+cd "$tempdir"
+git clone "$SOURCE_REPO" --depth=1
+cd kadwsettings
+./install.sh /var/usrlocal/
+cd /
+rm -r "$tempdir"
